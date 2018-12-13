cd3b139a30f7073ad67b66e656c303706675928a
xiong <313398439@qq.com>
2018-12-13 22:13:13 +0800

    init

---
+++ src/main/java/com/nju/campusqa/campusqa/CampusqaApplication.java
@@ -0,0 +1,14 @@
+package com.nju.campusqa.campusqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusqaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusqaApplication.class, args);
+    }
+
+}
+
