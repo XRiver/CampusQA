862698dd85dae2bb6e0bbd99b9c2d04d1a4dad2c
RiverXu <sdxujianghe@126.com>
2018-12-16 20:38:02 +0800

    User.authorization

--- src/main/java/com/nju/campusqa/campusqa/controller/UserController.java
+++ src/main/java/com/nju/campusqa/campusqa/controller/UserController.java
@@ -11,10 +11,7 @@ import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
@@ -29,6 +26,7 @@ public class UserController {
 
     public static final String NICKNAME_COL = "nickName";
     public static final String AVATAR_COL = "avatarUrl";
+    public static final String ROLE_COL = "role";
 
     @Autowired
     UserService userService;
@@ -37,15 +35,15 @@ public class UserController {
     private MongoTemplate mongoTemplate;
 
     @ResponseBody
-    @GetMapping("/api/signup")
-    public Response<User> signUp(@RequestBody Map<String,Object>params) {
+    @PostMapping("/api/signup")
+    public Response<User> signUp(@RequestBody Map<String, Object> params) {
 
-        String code = (String)params.get("code"),
-                avatarUrl = (String)params.get(AVATAR_COL),
-                nickName = (String)params.get(NICKNAME_COL);
+        String code = (String) params.get("code"),
+                avatarUrl = (String) params.get(AVATAR_COL),
+                nickName = (String) params.get(NICKNAME_COL);
 
         RestTemplate restTemplate = new RestTemplate();
-        String str =  restTemplate.getForObject("https://api.weixin.qq.com/sns/jscode2session?appid=wx8271f" +
+        String str = restTemplate.getForObject("https://api.weixin.qq.com/sns/jscode2session?appid=wx8271f" +
                 "84618387e52&secret=31cb337d5e72dc4c1eb3194a1e6caef4&js_code={code}&grant_type=authorizatio" +
                 "n_code", String.class, code);
         String[] strs = str.split("\"openid\":\"");
@@ -55,7 +53,7 @@ public class UserController {
         Query query = Query.query(criteria);
         User queryRet = mongoTemplate.findOne(query, User.class);
 
-        if (queryRet == null){ // Create new user
+        if (queryRet == null) { // Create new user
 
             User newUser = new User();
             newUser.setOpenId(openId);
@@ -66,21 +64,44 @@ public class UserController {
             newUser.setRole(0);
             newUser.setBan(null);
 
-            queryRet =  mongoTemplate.save(newUser,"user");
+            queryRet = mongoTemplate.save(newUser, "user");
 
         } else { // Update nickName and avatarUrl
 
             Criteria critUpdate = Criteria.where("id").is(queryRet.getId());
             Query queryUpdate = Query.query(critUpdate);
-            Update update = Update.update(AVATAR_COL,avatarUrl);
-            update.set(NICKNAME_COL,nickName);
+            Update update = Update.update(AVATAR_COL, avatarUrl);
+            update.set(NICKNAME_COL, nickName);
 
-            UpdateResult updateRet = mongoTemplate.updateFirst(query,update,User.class);
+            UpdateResult updateRet = mongoTemplate.updateFirst(queryUpdate, update, User.class);
             queryRet = mongoTemplate.findOne(query, User.class); // Re-query the User object.
 
         }
 
         return Response.createBySuccess(queryRet);
+    }
+
+    @ResponseBody
+    @PostMapping("/api/authorization")
+    public Response<Object> grantAdmin(@RequestBody Map<String, Object> params) {
+
+        String userId = (String) params.get("userId"),
+                password = (String) params.get("password");
+
+        if("888888".equals(password)) {
+            if(userService.findOne(userId) != null) {
+
+                Criteria critUpdate = Criteria.where("id").is(userId);
+                Query queryUpdate = Query.query(critUpdate);
+                Update update = Update.update(ROLE_COL,1);
+
+                UpdateResult updateRet = mongoTemplate.updateFirst(queryUpdate, update, User.class);
+            }
+
+            return Response.createBySuccess(null);
+        } else {
+            return Response.createByNeedAuthority(null);
+        }
     }
 
 }
